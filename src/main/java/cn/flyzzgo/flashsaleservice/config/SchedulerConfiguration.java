8620d7fe7cabe8f7b807e0e4211f06112315c289
flyzz-z <1353530978@qq.com>
2022-02-28 23:32:59 +0800

    秒杀品定时周期预热完成

---
+++ src/main/java/cn/flyzzgo/flashsaleservice/config/SchedulerConfiguration.java
@@ -0,0 +1,12 @@
+package cn.flyzzgo.flashsaleservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Flyzz
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfiguration {
+}
---
+++ src/main/java/cn/flyzzgo/flashsaleservice/scheduler/FlashItemWarmUpScheduler.java
@@ -0,0 +1,44 @@
+package cn.flyzzgo.flashsaleservice.scheduler;
+
+import cn.flyzzgo.flashsaleservice.model.dto.FlashItemDto;
+import cn.flyzzgo.flashsaleservice.model.entity.FlashItemDo;
+import cn.flyzzgo.flashsaleservice.model.response.Response;
+import cn.flyzzgo.flashsaleservice.model.response.SingleResponse;
+import cn.flyzzgo.flashsaleservice.service.FlashItemService;
+import cn.flyzzgo.flashsaleservice.service.ItemStockService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Flyzz
+ */
+@Component
+public class FlashItemWarmUpScheduler {
+
+    @Resource
+    private FlashItemService flashItemService;
+
+    @Resource
+    private ItemStockService itemStockService;
+
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void warmUpFlashItemTask() {
+        Response response = flashItemService.getNotWarmUpItemList(30L);
+        if(!response.isSuccess()) {
+            return;
+        }
+
+        List<FlashItemDto> flashItemDoList = (List<FlashItemDto>) ((SingleResponse)response).getData();
+        if(CollectionUtils.isEmpty(flashItemDoList)) {
+            return;
+        }
+
+        flashItemDoList.forEach(item ->{
+            itemStockService.initItemStock(item.getId());
+        });
+    }
+}
