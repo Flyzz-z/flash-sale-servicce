621c3e51d14c3a738f685f00ac3a42b9c39fd8f0
flyzz-z <1353530978@qq.com>
2022-02-28 23:33:19 +0800

    添加全局异常处理

---
+++ src/main/java/cn/flyzzgo/flashsaleservice/handler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package cn.flyzzgo.flashsaleservice.handler;
+
+import cn.flyzzgo.flashsaleservice.constant.enums.ErrorCode;
+import cn.flyzzgo.flashsaleservice.model.exception.BizException;
+import cn.flyzzgo.flashsaleservice.model.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Flyzz
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = BizException.class)
+    public Response bizExceptionHandler(HttpServletRequest httpServletRequest, BizException e) {
+        log.info(e.getErrCode() + e.getMessage());
+        return Response.buildFailure(e.getErrCode(),e.getMessage());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public Response ExceptionHandler(HttpServletRequest httpServletRequest, Exception e) {
+        log.info(e.getMessage());
+        return Response.buildFailure(ErrorCode.SERVER_ERROR);
+    }
+}
